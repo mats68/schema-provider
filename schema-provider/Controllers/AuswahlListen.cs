d72ea39618ea6cac8b2f715e996145ddb5194352
Matthias Thaler <matthias.thaler@brunnerinformatik.ch>
2020-09-22 14:42:05 +0200

    auswahlliste api

---
+++ schema-provider/Controllers/AuswahlListen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace schema_provider.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuswahlListen : ControllerBase
+	{
+		private static readonly Dictionary<String, String> _auswahlListe_VNBs =
+			new Dictionary<String, String>()
+			{
+				{ "BKW_DE", "BKW (de)" },
+				{ "Primeo", "Primeo" },
+				{ "Onyx", "Onyx" }
+			};
+
+		private static readonly Dictionary<String, String> _auswahlListe_Mitarbeiter =
+			new Dictionary<String, String>()
+			{
+				{ "THMA", "Thaler Matthias" },
+				{ "BRDA", "Brunner Daniel" },
+				{ "KARO", "Käch Ronnie" }
+			};
+
+		private static readonly Dictionary<String, Dictionary<String, String>> _auswahlListen =
+			new Dictionary<string, Dictionary<string, string>>()
+			{
+				{ "vnb", _auswahlListe_VNBs },
+				{ "mitarbeiter", _auswahlListe_Mitarbeiter }
+			};
+
+		[HttpGet()]
+		[Route("api/[controller]/{name}")]
+		public async Task<IActionResult> holeAuswahlListe(string name)
+		{
+			name = name?.ToLower();
+
+			JObject antwort = null;
+			HttpStatusCode antwortCode;
+
+			if(_auswahlListen.ContainsKey(name))
+			{
+				antwortCode = HttpStatusCode.OK;
+				antwort =	
+					JObject.FromObject(
+						new
+						{
+							Name = name,
+							Daten = _auswahlListen[name]
+						}
+					);
+
+			}
+			else
+			{
+				antwortCode = HttpStatusCode.NotFound;
+				antwort =
+					JObject.FromObject(
+						new
+						{
+							Name = name,
+							Daten = (Dictionary<String, String>)null
+						}
+					);
+			}
+
+			return new JsonNetResult(antwort, antwortCode);
+		}
+	}
+}
---
+++ schema-provider/JsonNetResult.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schema_provider
+{
+   public class JsonNetResult : ActionResult
+   {
+      private readonly JObject _data;
+      private readonly HttpStatusCode _code;
+
+      public JsonNetResult(JObject data, HttpStatusCode status = HttpStatusCode.OK)
+      {
+         _data = data;
+         _code = status;   
+      }
+
+		public override void ExecuteResult(ActionContext context)
+		{
+         var response = context.HttpContext.Response;
+
+         response.StatusCode = (int)_code;
+         response.ContentType = "application/json";
+         response.BodyWriter.WriteAsync(
+            Encoding.UTF8.GetBytes(
+               _data.ToString(Newtonsoft.Json.Formatting.None)
+            )
+         );
+      }
+   }
+}
